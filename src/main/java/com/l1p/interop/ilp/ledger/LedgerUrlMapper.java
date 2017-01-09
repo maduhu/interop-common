9395dc59173c24a4948e9e56393b999382e6445e
Honain Khan <honaink@gmail.com>
2017-01-09 17:01:47 -0600

    Updated code for Ledger Url Mapping.  Based on new use cased, the mapper handle more case in a general method

--- src/main/java/com/l1p/interop/ilp/ledger/LedgerUrlMapper.java
+++ src/main/java/com/l1p/interop/ilp/ledger/LedgerUrlMapper.java
@@ -3,33 +3,93 @@ package com.l1p.interop.ilp.ledger;
 import com.l1p.interop.ilp.ledger.domain.Credit;
 import com.l1p.interop.ilp.ledger.domain.Debit;
 import com.l1p.interop.ilp.ledger.domain.Transfer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.net.MalformedURLException;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Maps Ledger, Id, Account urls from dfsp specfic to ledger adapter specific urls
  */
 public class LedgerUrlMapper {
+  private static final Logger log = LoggerFactory.getLogger(LedgerUrlMapper.class);
 
-  private final String urlRemappingRegex;
-  private final String urlLedgerAdapterReplacementUrl;
+  private final String ledgerAdapterUrlMappingRegex;
+  private final String ledgerAdapterReplacementUrl;
+  private final String actualLedgerUrlMappingRegex;
+  private final String actualLedgerReplacementUrl;
 
-  public LedgerUrlMapper(String urlRemappingRegex, String urlLedgerAdapterReplacementUrl) {
-    this.urlRemappingRegex = urlRemappingRegex;
-    this.urlLedgerAdapterReplacementUrl = urlLedgerAdapterReplacementUrl;
+
+  public LedgerUrlMapper(String ledgerAdapterUrlMappingRegex, String ledgerAdapterReplacementUrl, String actualLedgerUrlMappingRegex, String actualLedgerReplacementUrl) {
+    this.ledgerAdapterUrlMappingRegex = ledgerAdapterUrlMappingRegex;
+    this.ledgerAdapterReplacementUrl = ledgerAdapterReplacementUrl;
+    this.actualLedgerUrlMappingRegex = actualLedgerUrlMappingRegex;
+    this.actualLedgerReplacementUrl = actualLedgerReplacementUrl;
   }
 
   public void mapUrlToIlpAdapterLedger(Transfer transfer) throws MalformedURLException {
-    transfer.setId(transfer.getId().replaceFirst(urlRemappingRegex, urlLedgerAdapterReplacementUrl));
-    transfer.setLedger(transfer.getLedger().replaceFirst(urlRemappingRegex, urlLedgerAdapterReplacementUrl));
+    transfer.setId(transfer.getId().replaceFirst(ledgerAdapterUrlMappingRegex, ledgerAdapterReplacementUrl));
+    transfer.setLedger(transfer.getLedger().replaceFirst(ledgerAdapterUrlMappingRegex, ledgerAdapterReplacementUrl));
 
     for (Credit credit : transfer.getCredits()) {
-      credit.setAccount(credit.getAccount().replaceFirst(urlRemappingRegex, urlLedgerAdapterReplacementUrl));
+      credit.setAccount(credit.getAccount().replaceFirst(ledgerAdapterUrlMappingRegex, ledgerAdapterReplacementUrl));
     }
 
     for (Debit debit : transfer.getDebits()) {
-      debit.setAccount(debit.getAccount().replaceFirst(urlRemappingRegex, urlLedgerAdapterReplacementUrl));
+      debit.setAccount(debit.getAccount().replaceFirst(ledgerAdapterUrlMappingRegex, ledgerAdapterReplacementUrl));
     }
+  }
+
+  public void mapUrlToLedgerAdapterUrl(final String[] fields, final Map<String, Object> map) {
+    mapUrls(fields, map, ledgerAdapterUrlMappingRegex, ledgerAdapterReplacementUrl);
+  }
+
+  public void mapUrlToActualLedgerUrl(final String[] fields, final Map<String, Object> map) {
+    mapUrls(fields, map, actualLedgerUrlMappingRegex, actualLedgerReplacementUrl);
+  }
+
+  private void mapUrls(final String[] fields, final Map<String, Object> targetMap, final String regex, final String replacementUrl) {
+    for (String field : fields) { // iterate through all fields and map url
+      final String[] keys = field.split("\\.");
+      Map<String, Object> currentMap = targetMap;
+      for (int i = 0; i < keys.length; i++) {
+        String key = keys[i];
+        final Object currentValue = currentMap.get(key);
+        if (currentValue == null) {
+          log.warn("field: {} not found in the map for URL transform", field);
+          break;
+        }
+        if (currentValue instanceof Map) {
+          currentMap = (Map) currentValue;
+        } else if (currentValue instanceof String) {
+          final String mappedUrl = ((String) currentValue).replaceFirst(regex, replacementUrl);
+          currentMap.put(key, mappedUrl);
+          break;
+        } else if (currentValue instanceof List) {
+          List fieldList = (List) currentValue;
+          for (Object fieldFromList : fieldList) {
+            if (fieldFromList instanceof Map) {
+              String remainingFieldName = extractRemainingField(field, i + 1);
+              mapUrls(new String[] {remainingFieldName}, (Map) fieldFromList, regex, replacementUrl);
+            }
+          }
+          break;
+        } else {  // unexpected type
+          log.error("Unexpected type:{} found in target Map while mapping to ledger URL for field:{}", currentValue.getClass(), field);
+          break;
+        }
+      }
+    }
+  }
+
+  private String extractRemainingField(final String field, final int startingToken) {
+    String remainingString = field;
+    for(int i = 0; i < startingToken; i++ ) {
+      remainingString = remainingString.substring(remainingString.indexOf('.') + 1);
+    }
+    return remainingString;
   }
 
 }
---
+++ src/test/java/com/l1p/interop/ilp/ledger/LedgerUrlMapperTest.java
@@ -0,0 +1,128 @@
+package com.l1p.interop.ilp.ledger;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.l1p.interop.ilp.ledger.domain.Transfer;
+import org.junit.Test;
+import org.springframework.util.Assert;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+public class LedgerUrlMapperTest {
+
+  @Test
+  public void testJsonConversion() throws Exception {
+    ObjectMapper mapper = new ObjectMapper();
+    String value = "{\"id\":\"http://usd-ledger.example/transfers/3a2a1d9e-8640-4d2d-b06c-84f2cd613204\",\"ledger\":\"http://usd-ledger.example\",\"debits\":[{\"account\":\"http://usd-ledger.example/accounts/alice\",\"amount\":\"50\",\"authorized\":true}],\"credits\":[{\"account\":\"http://usd-ledger.example/accounts/bob\",\"amount\":\"50\"}],\"execution_condition\":\"cc:0:3:8ZdpKBDUV-KX_OnFZTsCWB_5mlCFI3DynX5f5H2dN-Y:2\",\"expires_at\":\"2015-06-16T00:00:01.000Z\",\"state\":\"prepared\"}";
+    String value2 = "{\n" +
+        "  \"id\": \"http://ec2-35-163-231-111.us-west-2.compute.amazonaws.com:8088/ledger/transfers/6ca071e5-09e2-4e20-8843-d3e8a0dd1f55\",\n" +
+        "  \"ledger\": \"http://ec2-35-163-231-111.us-west-2.compute.amazonaws.com:8088/ledger\",\n" +
+        "  \"debits\": [\n" +
+        "    {\n" +
+        "      \"account\": \"http://ec2-35-163-231-111.us-west-2.compute.amazonaws.com:8088/ledger/accounts/dfsp1connector\",\n" +
+        "      \"memo\": {\n" +
+        "        \"source_transfer_ledger\": \"levelone.ist.\",\n" +
+        "        \"source_transfer_id\": \"e829a9bf-b87b-4d84-87f3-18dc467ae7c7\"\n" +
+        "      },\n" +
+        "      \"amount\": \"7.00\"\n" +
+        "    }\n" +
+        "  ],\n" +
+        "  \"credits\": [\n" +
+        "    {\n" +
+        "      \"account\": \"http://ec2-35-163-231-111.us-west-2.compute.amazonaws.com:8088/ledger/accounts/merchant\",\n" +
+        "      \"memo\": {\n" +
+        "        \"ilp_header\": {\n" +
+        "          \"account\": \"levelone.dfsp1.merchant.4bf0ee9d-1244-4e77-8951-f05a07f4f8a1\",\n" +
+        "          \"amount\": \"7\",\n" +
+        "          \"data\": {\n" +
+        "            \"data\": {\n" +
+        "              \"memo\": {\n" +
+        "                \"fee\": 0,\n" +
+        "                \"transferCode\": \"invoice\",\n" +
+        "                \"debitName\": \"bob\",\n" +
+        "                \"creditName\": \"merchant\"\n" +
+        "              }\n" +
+        "            },\n" +
+        "            \"expires_at\": \"2016-12-23T13:59:26.173Z\"\n" +
+        "          }\n" +
+        "        }\n" +
+        "      },\n" +
+        "      \"amount\": \"7.00\"\n" +
+        "    }\n" +
+        "  ],\n" +
+        "  \"execution_condition\": \"cc:0:3:iLS9w-CiypRGXk3VmvJsKlcfOnH0kXvdxL5EIcdjSjI:32\",\n" +
+        "  \"cancellation_condition\": null,\n" +
+        "  \"state\": \"executed\",\n" +
+        "  \"expires_at\": \"2016-12-23T13:59:24.173Z\"\n" +
+        "}\n";
+    String value3 = "\"{   \"id\": \"http://ec2-35-163-231-111.us-west-2.compute.amazonaws.com:8088/ledger/transfers/6ca071e5-09e2-4e20-8843-d3e8a0dd1f55\",   \"ledger\": \"http://ec2-35-163-231-111.us-west-2.compute.amazonaws.com:8088/ledger\",   \"debits\": [     {       \"account\": \"http://ec2-35-163-231-111.us-west-2.compute.amazonaws.com:8088/ledger/accounts/dfsp1connector\",       \"memo\": {         \"source_transfer_ledger\": \"levelone.ist.\",         \"source_transfer_id\": \"e829a9bf-b87b-4d84-87f3-18dc467ae7c7\"       },       \"amount\": \"7.00\"     }   ],   \"credits\": [     {       \"account\": \"http://ec2-35-163-231-111.us-west-2.compute.amazonaws.com:8088/ledger/accounts/merchant\",       \"memo\": {         \"ilp_header\": {           \"account\": \"levelone.dfsp1.merchant.4bf0ee9d-1244-4e77-8951-f05a07f4f8a1\",           \"amount\": \"7\",           \"data\": {             \"data\": {               \"memo\": {                 \"fee\": 0,                 \"transferCode\": \"invoice\",                 \"debitName\": \"bob\",                 \"creditName\": \"merchant\"               }             },             \"expires_at\": \"2016-12-23T13:59:26.173Z\"           }         }       },       \"amount\": \"7.00\"     }   ],   \"execution_condition\": \"cc:0:3:iLS9w-CiypRGXk3VmvJsKlcfOnH0kXvdxL5EIcdjSjI:32\",   \"cancellation_condition\": null,   \"state\": \"executed\",   \"expires_at\": \"2016-12-23T13:59:24.173Z\" }\"";
+    final Transfer transfer = mapper.readValue(value3, Transfer.class);
+    Assert.notNull(transfer.getExecutionCondition());
+  }
+
+  @Test
+  public void testReplaceWithRegex() {
+    String srcUrl = "http://ec2-35-163-231-111.us-west-2.compute.amazonaws.com:8088/ledger/transfers/6ca071e5-09e2-4e20-8843-d3e8a0dd1f55";
+    final String result = srcUrl.replaceFirst(".*/ledger/", "/banana/");
+    assertEquals("/banana/transfers/6ca071e5-09e2-4e20-8843-d3e8a0dd1f55", result);
+  }
+
+  @Test
+  public void testLedgerUrlConversion() throws IOException {
+    String transferJson = "{\n" +
+        "  \"id\": \"http://ec2-35-163-231-111.us-west-2.compute.amazonaws.com:8088/ledger/transfers/6ca071e5-09e2-4e20-8843-d3e8a0dd1f55\",\n" +
+        "  \"ledger\": \"http://ec2-35-163-231-111.us-west-2.compute.amazonaws.com:8088/ledger\",\n" +
+        "  \"debits\": [\n" +
+        "    {\n" +
+        "      \"account\": \"http://ec2-35-163-231-111.us-west-2.compute.amazonaws.com:8088/ledger/accounts/dfsp1connector\",\n" +
+        "      \"memo\": {\n" +
+        "        \"source_transfer_ledger\": \"levelone.ist.\",\n" +
+        "        \"source_transfer_id\": \"e829a9bf-b87b-4d84-87f3-18dc467ae7c7\"\n" +
+        "      },\n" +
+        "      \"amount\": \"7.00\"\n" +
+        "    }\n" +
+        "  ],\n" +
+        "  \"credits\": [\n" +
+        "    {\n" +
+        "      \"account\": \"http://ec2-35-163-231-111.us-west-2.compute.amazonaws.com:8088/ledger/accounts/merchant\",\n" +
+        "      \"memo\": {\n" +
+        "        \"ilp_header\": {\n" +
+        "          \"account\": \"levelone.dfsp1.merchant.4bf0ee9d-1244-4e77-8951-f05a07f4f8a1\",\n" +
+        "          \"amount\": \"7\",\n" +
+        "          \"data\": {\n" +
+        "            \"data\": {\n" +
+        "              \"memo\": {\n" +
+        "                \"fee\": 0,\n" +
+        "                \"transferCode\": \"invoice\",\n" +
+        "                \"debitName\": \"bob\",\n" +
+        "                \"creditName\": \"merchant\"\n" +
+        "              }\n" +
+        "            },\n" +
+        "            \"expires_at\": \"2016-12-23T13:59:26.173Z\"\n" +
+        "          }\n" +
+        "        }\n" +
+        "      },\n" +
+        "      \"amount\": \"7.00\"\n" +
+        "    }\n" +
+        "  ],\n" +
+        "  \"execution_condition\": \"cc:0:3:iLS9w-CiypRGXk3VmvJsKlcfOnH0kXvdxL5EIcdjSjI:32\",\n" +
+        "  \"cancellation_condition\": null,\n" +
+        "  \"state\": \"executed\",\n" +
+        "  \"expires_at\": \"2016-12-23T13:59:24.173Z\"\n" +
+        "}\n";
+    ObjectMapper mapper = new ObjectMapper();
+    Map<String, Object> interopGenericMap = mapper.readValue(transferJson, new TypeReference<Map<String, Object>>() { });
+    final LedgerUrlMapper ledgerUrlMapper = new LedgerUrlMapper(".*/ledger", "http://ec2-35-163-231-111.us-west-2.compute.amazonaws.com:8081/ilp/ledger/v1", ".*/v1", "http://ec2-35-163-231-111.us-west-2.compute.amazonaws.com:8088/ledger");
+    final String[] fields = {"id", "ledger", "debits.account", "credits.account", "sourceAccount", "account"};
+    String actualLedgerUrlId = (String) interopGenericMap.get("id");
+    ledgerUrlMapper.mapUrlToLedgerAdapterUrl(fields, interopGenericMap);
+    assertNotEquals(actualLedgerUrlId, interopGenericMap.get("id"));
+    ledgerUrlMapper.mapUrlToActualLedgerUrl(fields, interopGenericMap);
+    assertEquals(actualLedgerUrlId, interopGenericMap.get("id"));
+  }
+}
