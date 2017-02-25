1006189f70267fb4cd0f04ed59b33b68854f89e6
Murthy Kakarlamudi <murthy@modusbox.com>
2017-02-24 19:23:58 -0500

    added propose_at attribute

--- src/main/java/com/l1p/interop/ilp/ledger/domain/Timeline.java
+++ src/main/java/com/l1p/interop/ilp/ledger/domain/Timeline.java
@@ -1,47 +1,62 @@
 package com.l1p.interop.ilp.ledger.domain;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.Date;
 
 public class Timeline {
-  @JsonProperty(value = "prepared_at")
-  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
-  private Date preparedAt;
 
-  @JsonProperty(value = "executed_at")
-  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
-  private Date executedAt;
+	@JsonIgnore
+	@JsonProperty(value = "proposed_at")
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+	private Date proposedAt;
 
-  @JsonProperty(value = "rejected_at")
-  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
-  private Date rejectedAt;
+	@JsonProperty(value = "prepared_at")
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+	private Date preparedAt;
 
-  public Timeline() {
-  }
+	@JsonProperty(value = "executed_at")
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+	private Date executedAt;
 
-  public Date getPreparedAt() {
-    return preparedAt;
-  }
+	@JsonProperty(value = "rejected_at")
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+	private Date rejectedAt;
 
-  public void setPreparedAt(Date preparedAt) {
-    this.preparedAt = preparedAt;
-  }
+	public Timeline() {
+	}
 
-  public Date getExecutedAt() {
-    return executedAt;
-  }
+	public Date getProposedAt() {
+		return proposedAt;
+	}
 
-  public void setExecutedAt(Date executedAt) {
-    this.executedAt = executedAt;
-  }
+	public void setProposedAt(Date proposedAt) {
+		this.proposedAt = proposedAt;
+	}
 
-  public Date getRejectedAt() {
-    return rejectedAt;
-  }
+	public Date getPreparedAt() {
+		return preparedAt;
+	}
 
-  public void setRejectedAt(Date rejectedAt) {
-    this.rejectedAt = rejectedAt;
-  }
+	public void setPreparedAt(Date preparedAt) {
+		this.preparedAt = preparedAt;
+	}
+
+	public Date getExecutedAt() {
+		return executedAt;
+	}
+
+	public void setExecutedAt(Date executedAt) {
+		this.executedAt = executedAt;
+	}
+
+	public Date getRejectedAt() {
+		return rejectedAt;
+	}
+
+	public void setRejectedAt(Date rejectedAt) {
+		this.rejectedAt = rejectedAt;
+	}
 }
