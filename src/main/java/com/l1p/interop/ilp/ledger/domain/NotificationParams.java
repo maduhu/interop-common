bb3f03f37e358338aadcea75ba9f98a3d06c2625
Murthy Kakarlamudi <murthy@modusbox.com>
2017-02-25 20:33:39 -0500

    added code to populate related resources in params

--- src/main/java/com/l1p/interop/ilp/ledger/domain/NotificationParams.java
+++ src/main/java/com/l1p/interop/ilp/ledger/domain/NotificationParams.java
@@ -2,30 +2,38 @@ package com.l1p.interop.ilp.ledger.domain;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.HashMap;
 import java.util.Map;
 
 abstract public class NotificationParams {
-  private String id;
-  private Object resource;
-  private String event;
+	private String id;
+	private Object resource;
+	private String event;
+	@JsonProperty(value = "related_resources")
+	private HashMap<String, String> relatedResources;
+	
+	public NotificationParams(String event, Object resource) {
+		this.resource = resource;
+		this.event = event;
+	}
 
-  public NotificationParams(String event, Object resource) {
-    this.resource = resource;
-    this.event = event;
-  }
+	public String getEvent() {
+		return event;
+	};
 
-  @JsonProperty(value = "related_resources")
-  private Map<String, String> relatedResources;
+	public Object getResource() {
+		return resource;
+	}
 
-  public String getEvent() {
-    return event;
-  };
+	public void setResource(Object resource) {
+		this.resource = resource;
+	}
 
-  public Object getResource() {
-    return resource;
-  }
+	public Map<String, String> getRelatedResources() {
+		return relatedResources;
+	}
 
-  public void setResource(Object resource) {
-    this.resource = resource;
-  }
+	public void setRelatedResources(HashMap<String, String> relatedResources) {
+		this.relatedResources = relatedResources;
+	}
 }
