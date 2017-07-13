d7c5aceeed9aae3b8d599c2994e87267ba61cf95
Murthy Kakarlamudi <murthy@modusbox.com>
2017-07-13 10:41:41 -0400

    added id, ilp fields for Message object

--- src/main/java/com/l1p/interop/ilp/ledger/domain/Message.java
+++ src/main/java/com/l1p/interop/ilp/ledger/domain/Message.java
@@ -1,40 +1,71 @@
 package com.l1p.interop.ilp.ledger.domain;
 
 public class Message {
-  private String ledger;
-  private String from;
-  private String to;
-  private Object data;
+	
+	private String id;
+	private String ilp;
+	private String ledger;
+	private String from;
+	private String to;
+	private Object data;
 
-  public String getLedger() {
-    return ledger;
-  }
+	public String getLedger() {
+		return ledger;
+	}
 
-  public void setLedger(String ledger) {
-    this.ledger = ledger;
-  }
+	public void setLedger(String ledger) {
+		this.ledger = ledger;
+	}
 
-  public String getFrom() {
-    return from;
-  }
+	public String getFrom() {
+		return from;
+	}
 
-  public void setFrom(String from) {
-    this.from = from;
-  }
+	public void setFrom(String from) {
+		this.from = from;
+	}
 
-  public String getTo() {
-    return to;
-  }
+	public String getTo() {
+		return to;
+	}
 
-  public void setTo(String to) {
-    this.to = to;
-  }
+	public void setTo(String to) {
+		this.to = to;
+	}
 
-  public Object getData() {
-    return data;
-  }
+	public Object getData() {
+		return data;
+	}
 
-  public void setData(Object data) {
-    this.data = data;
-  }
+	public void setData(Object data) {
+		this.data = data;
+	}
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the ilp
+	 */
+	public String getIlp() {
+		return ilp;
+	}
+
+	/**
+	 * @param ilp the ilp to set
+	 */
+	public void setIlp(String ilp) {
+		this.ilp = ilp;
+	}
+
 }
